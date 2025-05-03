97b44e7d382140d87def20c3ef7337f4f5a1d2e6
CPea <cpea2506@gmail.com>
2025-05-03 18:00:39 +0700

    Finish chapter 1

--- src/chapter1/main.cpp
+++ src/chapter1/main.cpp
@@ -1,3 +1,12 @@
-int main(int argc, char *argv[]) {
+#include <iostream>
+
+int main() {
+    std::cout << "Enter an integer: ";
+
+    int num{};
+    std::cin >> num;
+
+    std::cout << "Double that number is: " << num * 2 << '\n';
+
     return 0;
 }
